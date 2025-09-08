8138c3ce5cff587bdf5e4b6ffc333c07293daaea
lunarcleint <xenovhub@gmail.com>
2025-09-08 15:52:22 -0700

    laptop gonna combust

--- hscript/Parser.hx
+++ hscript/Parser.hx
@@ -1,5 +1,7 @@
 package hscript;
 
+import hscript.Expr.VariableType;
+import hscript.Lexer.LKeyword;
 import hscript.Expr.ExprDef;
 import hscript.Lexer.LTokenPos;
 import hscript.Error.ErrorDef;
@@ -21,6 +23,43 @@ class Parser {
         this.origin = origin ?? "";
     }
 
+    private inline function parseExpr():Expr {
+        return null;
+    }
+
+    private inline function parseKeyword(keyword:LKeyword) {
+        return switch (keyword) {
+            case BREAK: create(EBreak);
+            case CONTINUE: create(EContinue);
+            case VAR | INLINE | FINAL: 
+                var variableName:String = parseIdent(readToken());
+                if(maybe(LTColon)) 
+                    parseIdent(readToken()); // allow types
+
+                var assign:Expr = null;
+                if (maybe(LTOp(ASSIGN)))
+                    assign = parseExpr();
+
+                create(EVar(variableID(variableName), assign, false, false));
+            default: null;
+        }
+    }
+
+    private inline function parseIdent(token:LToken):String {
+        switch (token) {
+            case LTIdentifier(identifier): return identifier;
+            default: unexpected(token); return null;
+        }
+    }
+
+    private inline function variableID(string:String):VariableType {
+        var varID:VariableType = variablesList.indexOf(string);
+        if (varID == -1) {
+            variablesList.push(string);
+            return variablesList.length-1;
+        } else return varID;
+    }
+
     private inline function create(expr:ExprDef) {
         return {
             expr: expr,
@@ -30,23 +69,26 @@ class Parser {
         };
     }
 
-    private inline function maybe(expected:LToken) {
-        var testToken:LToken = readToken().token;
-        if (!Type.enumEq(expected, testToken)) token--;
+    private inline function maybe(expected:LToken):Bool {
+        var testToken:LToken = readToken();
+        if (!Type.enumEq(expected, testToken)) {
+            token--;
+            return false;
+        } else return true;
     }
 
     private inline function deepEnsure(expected:LToken) {
-        var testToken:LToken = readToken().token;
+        var testToken:LToken = readToken();
         if (!Type.enumEq(expected, testToken)) unexpected(testToken);
     }
 
     private inline function ensure(expected:LToken) {
-        var testToken:LToken = readToken().token;
+        var testToken:LToken = readToken();
         if (expected != testToken) unexpected(testToken);
     }
 
-    private inline function readToken():LTokenPos {
-        return tokens[token++];
+    private inline function readToken():LToken {
+        return tokens[token++].token;
     }
 
     private inline function unexpected(token:LToken) {
