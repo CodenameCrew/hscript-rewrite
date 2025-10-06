524ca9fa3a1a9750376e68829f8973a7c85be657
lunarcleint <xenovhub@gmail.com>
2025-10-06 15:00:21 -0700

    s

--- hscript/Interp.hx
+++ hscript/Interp.hx
@@ -577,9 +577,8 @@ class Interp implements IInterp {
     }
 
     private inline function interpNew(className:VariableType, args:Array<Dynamic>):Dynamic {
-        trace(variableNames[className]);
         var classType = if (variablesDeclared[className]) variablesValues[className] else Type.resolveClass(variableNames[className]);
-        trace(variablesDeclared[className], variablesValues[className]);
+        trace(variableNames[className], variablesDeclared[className], classType);
         if (classType == null) classType = resolveGlobal(className);
 
         var params:Array<Dynamic> = [for (arg in args) interpExpr(arg)];
