6bef21d4b45e81380a5411d01bffca8789ef0786
Jaime Humberto Macias Bustamante <jamextreme140@outlook.com>
2025-10-17 01:23:07 -0700

    Update Main.hx

--- Main.hx
+++ Main.hx
@@ -1,44 +1,11 @@
 package;
 
-import hscript.utils.Printer;
 import hscript.Ast.Expr;
 import hscript.Interp;
 import hscript.Parser;
 
 class Main {
-	private static inline var code:String = "
-            var a:Int = 5;
-
-            function sum(a:Int) {
-                return a + 1;    
-            }
-            
-            a = sum(a);
-            trace(a);
-            var m = [1 => 'a', 2 => 'b', 3 => 'c'];
-            var b = m[1];
-            trace(b);
-            for(k => v in m) {
-                trace(k + ' -> ' + v);
-            }
-            var c = {
-                aa: ':3',
-                ab: function() {
-                    return 10;    
-                }
-            };
-            trace(c?.aa);
-            trace(c.ab());
-        ";
     public static function main() {
-        var parser = new Parser();
-        var expr = parser.parseString(code);
-        trace("Code: \n");
-        trace(Printer.toString(expr));
-        trace("Output:");
-        var interp = new Interp("Main.hx");
-        interp.execute(expr);
-        /*
         var parser = new Parser();
         var expr = parser.parseString('
             2 + 2;
@@ -46,6 +13,5 @@ class Main {
 
         var interp = new Interp("Main.hx");
         interp.execute(expr);
-        */
     }
 }
