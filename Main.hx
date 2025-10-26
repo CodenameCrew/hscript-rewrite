12ba542033e3714dd1ed66121d901eebe79b9eb7
lunarcleint <xenovhub@gmail.com>
2025-10-26 02:52:39 -0700

    Spent 2 hours on this bart simpson for nothing

--- Main.hx
+++ Main.hx
@@ -15,33 +15,20 @@ class Main {
     public static function main() {
         var parser = new Parser();
         var expr = parser.parseString('
-			var a = []; 
-			for (i in 0...1000) a.push(i * 2 + 1 / 6); 
-			if(true == true) a.push(1);
-			a[0];
+			var a = 3.5;
+			trace(a * 2);
+			trace(a / 2);
+
 		');
 
 		trace(ExprUtils.print(expr, true));
-		expr = ConstEval.eval(expr);
-		trace(ExprUtils.print(expr, true));
-
-		var comp:ByteCompiler = new ByteCompiler();
-		var byteCode:ByteChunk = comp.compile(expr);
-		trace(byteCode, byteCode.instructions.length);
-
-		Sys.println(BytesPrinter.print(byteCode));
+		var oexpr = ConstEval.eval(expr);
 
 		var interp:Interp = new Interp("Main.hx");
 		interp.errorHandler = (error:Error) -> {Sys.println(error);}
-
-		Sys.println("");
-		var vm:ByteVM = new ByteVM("Main.hx");
-		vm.errorHandler = (error:Error) -> {Sys.println(error);}
-
-		var time:Float = Timer.stamp();
-		for (i in 0...2000)
-			vm.execute(byteCode);
-		trace(Timer.stamp() - time);
-
+		interp.execute(expr);
+		interp.reset();
+		trace(ExprUtils.print(oexpr, true));
+		interp.execute(oexpr);
     }
 }
--- hscript/anaylzers/ConstEval.hx
+++ hscript/anaylzers/ConstEval.hx
@@ -12,13 +12,14 @@ using hscript.utils.ExprUtils;
 using hscript.Ast.ExprBinop;
 
 class ConstEval {
+    public static final SAFE_BITSHIFT_RANGE:Int = 30; // safe zone for 32 bit signed ints 
+
     public static function eval(expr:Expr):Expr {
         return new Expr(switch (expr.expr) {
             case EVar(name, init, isPublic, isStatic): EVar(name, if (init != null) eval(init) else null, isPublic, isStatic);
             case EParent(expr): EParent(eval(expr));
             case EBlock(exprs): EBlock([for (expr in exprs) eval(expr)].filter((expr:Expr) -> {return expr != null;}));
             case EField(expr, field, isSafe): EField(eval(expr), field, isSafe); 
-            case EUnop(op, isPrefix, expr): EUnop(op, isPrefix, eval(expr));
             case ECall(func, args): ECall(eval(func), [for (expr in args) eval(expr)]);
             case EWhile(cond, body): EWhile(eval(cond), eval(body));
             case EFor(varName, iterator, body): EFor(varName, eval(iterator), eval(body));
@@ -44,6 +45,19 @@ class ConstEval {
             case EMeta(name, args, expr): EMeta(name, [for (arg in args) eval(arg)], eval(expr));
             case EInfo(info, expr): EInfo(info, eval(expr));
             case EBreak | EConst(_) | EContinue | EIdent(_) | EImport(_): expr.expr; 
+            case EUnop(op, isPrefix, expr):
+                var optimizedExpr:Expr = eval(expr);
+                var exprConst:LConst = exprToConst(optimizedExpr);
+
+                if (exprConst != null && isPrefix) {
+                    switch (op) {
+                        case NEG: return new Expr(EConst(dynamicToConst(-StaticInterp.evaluateConst(exprConst))), optimizedExpr.line);
+                        case NEG_BIT: return new Expr(EConst(dynamicToConst(~StaticInterp.evaluateConst(exprConst))), optimizedExpr.line);
+                        case NOT: return new Expr(EConst(dynamicToConst(!StaticInterp.evaluateConst(exprConst))), optimizedExpr.line);
+                        default:
+                    }
+                }
+                EUnop(op, isPrefix, expr);
             case EIf(cond, thenExpr, elseExpr): 
                 var optimizedCond:Expr = eval(cond);
                 var condConst:LConst = exprToConst(optimizedCond);
@@ -65,12 +79,38 @@ class ConstEval {
 
                 var leftConst:LConst = exprToConst(leftOptimized);
                 var rightConst:LConst = exprToConst(rightOptimized);
+
                 if (leftConst != null && rightConst != null && !op.isAssign() && op != INTERVAL) {
                     var leftValue:Dynamic = StaticInterp.evaluateConst(leftConst);
                     var rightValue:Dynamic = StaticInterp.evaluateConst(rightConst);
 
                     return new Expr(EConst(dynamicToConst(StaticInterp.evaluateBinop(op, leftValue, rightValue))), left.line);
                 }
+
+                /**
+                 * Turn a multi/div by a power of 2 into a bitshift.
+                 * We can check both sides for mult for some reason I can't remb I learned it in 2nd grade??? -lunar
+                 * 
+                 * For example: a / 2 to a >> 1
+                 * For example: a a * 2 into a << 1 
+                if (rightConst != null || leftConst != null) {
+                    switch (op) {
+                        case DIV:
+                            var divInt:Int = constToInt(rightConst ?? LCInt(0));
+                            if (isPowerOf2(divInt)) { 
+                                var divRoot:Int = Std.int(Math.pow(divInt, 1 / 2));
+                                if (divRoot <= SAFE_BITSHIFT_RANGE) return new Expr(EBinop(SHR, left, new Expr(EConst(LCInt(divRoot)), expr.line)), expr.line);
+                            }
+                        case MULT:
+                            var multInt:Int = rightConst != null ? constToInt(rightConst) : constToInt(leftConst);
+                            if (isPowerOf2(multInt)) {
+                                var multRoot:Int = Std.int(Math.pow(multInt, 1 / 2));
+                                if (multRoot <= SAFE_BITSHIFT_RANGE) return new Expr(EBinop(SHL, rightConst != null ? left : right, new Expr(EConst(LCInt(multRoot)), expr.line)), expr.line);
+                            }
+                        default:
+                    }
+                }
+                 */
 
                 EBinop(op, leftOptimized, rightOptimized);
         }, expr.line);
@@ -93,5 +133,18 @@ class ConstEval {
             case TNull: LCNull;
             default: throw "Unknown type of constant: " + Type.typeof(value);
         }
+    }
+
+    public static function constToInt(const:LConst):Int {
+        return switch (const) {
+            case LCInt(int): int;
+            default: 0;
+        }
+    }
+
+
+    // https://stackoverflow.com/questions/600293/how-to-check-if-a-number-is-a-power-of-2
+    public static function isPowerOf2(value:Int):Bool {
+        return (value != 0) && ((value & (value - 1)) == 0);
     }
 }
