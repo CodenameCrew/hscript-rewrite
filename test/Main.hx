afe1c6e9f39455588ccc47842531ffa61600d297
lunarcleint <xenovhub@gmail.com>
2025-11-01 14:50:36 -0700

    move tests into test folder

---
+++ test/Main.hx
@@ -0,0 +1,26 @@
+package;
+
+import sys.io.File;
+import hscript.anaylzers.Inliner;
+import hscript.anaylzers.Unravel;
+import hscript.anaylzers.ConstEval;
+import hscript.Error;
+import hscript.Interp;
+import hscript.Parser;
+
+using hscript.utils.ExprUtils;
+
+class Main {
+    public static function main() {
+        var parser = new Parser();
+        var expr = parser.parseString(File.getContent("test.hx"));
+
+		expr = ConstEval.eval(expr);
+		expr = Unravel.eval(expr);
+		expr = Inliner.eval(expr);
+
+		var interp:Interp = new Interp("Main.hx");
+		interp.errorHandler = (error:Error) -> {Sys.println(error);}
+		interp.execute(expr);
+    }
+}
